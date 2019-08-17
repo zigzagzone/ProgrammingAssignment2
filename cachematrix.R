feeba07ca5331cf15206c2dbef981304ad8ed8cb
zigzagzone <zigzagzone@gmail.com>
2019-08-17 21:19:59 +0700

    Add more comment to explian the function.

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,26 @@
 
 ## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
+  
+  ## initial the data of 'inverseData' param
   inverseData <- NULL
+  
+  ## set function, to set matrix data and store in 'matrixData' param
   set <- function(ma) {
     matrixData <<- ma
     inverseData <<- NULL
   }
+  
+  ## get fuction, return 'matrixData'
   get <- function() matrixData
+  
+  ## setinverse function, set the inverse of matrix in 'inverseData' param
   setinverse <- function(inv) inverseData <<- inv
+  
+  ## getinverse function, return 'inverseData' that the inverse of 'matrixData'
   getinverse <- function() inverseData
+  
+  ## return a list contain set, get, setinverse, getinverse fuctions
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
@@ -22,14 +34,26 @@ makeCacheMatrix <- function(x = matrix()) {
 ## If the inverse has already been calculated (and the matrix has not changed),
 ## then cacheSolve should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  ## get the inverse data from special matrix object
   inverseData <- x$getinverse()
+  
+  ## check if it already cached
   if(!is.null(inverseData)) {
+    
+    ## found a cache!, return it
     message("getting cached data")
     return(inverseData)
   }
+  
+  ## cache not found, get matrixData from special matrix object
   matrixData <- x$get()
+  
+  ## call solve fuction to execute inverse of matrix
   inv <- solve(matrixData, ...)
+  
+  ## set inverse of matrix to special matrix object
   x$setinverse(inv)
+  
+  ## return inverse of matrix
   inv
 }
